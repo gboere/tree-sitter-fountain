4673e9bc6e4086e06f3df49123d40ad6a8091529
exclame <root@exclame.dev>
2024-06-24 16:52:04 -0400

    Fresh start
    Fresh start for grammar (teomporary)

--- grammar.js
+++ grammar.js
@@ -8,75 +8,24 @@ module.exports = grammar({
   ],
 
   rules: {
-    document: $ => repeat($._element),
-
-    _element: $ => choice(
-      $.scene_heading,
-      $.dialogue_block,
-      $.action,
-      $.transition,
-      $.section_heading,
-      $.note
     ),
 
-    scene_heading: $ => seq(
-      choice(
-        'INT.',
-        'EXT.',
-        'INT./EXT.',
-        'EST.'
-      ),
-      optional($.description),
+    ),
+
+    ),
+
+    ),
+
       '\n'
     ),
 
-    character: $ => seq(
-      /[A-Z][A-Z ]*[A-Z]/,
+    ),
+
+
       '\n'
-    ),
 
-    dialogue_block: $ => seq(
-      $.character,
-      repeat1(choice($.dialogue, $.parenthetical))
-    ),
 
-    dialogue: $ => seq(
-      $.line,
-      '\n'
-    ),
 
-    parenthetical: $ => seq(
-      '(',
-      /[^)]+/,
-      ')',
-      '\n'
-    ),
 
-    action: $ => seq(
-      $.line,
-      '\n'
-    ),
-
-    transition: $ => seq(
-      /[A-Z ]+:/,
-      '\n'
-    ),
-
-    section_heading: $ => seq(
-      repeat1('#'),
-      optional($.description),
-      '\n'
-    ),
-
-    note: $ => seq(
-      '[[',
-      /[^]]+/,
-      ']]',
-      '\n'
-    ),
-
-    line: $ => /[^\n]+/,
-
-    description: $ => /[^\n]+/
   }
 });
